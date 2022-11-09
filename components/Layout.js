1d94bee8efa73b5c9b2773baf5d0976dbeed78c0
JBUpark21 <bzpark@naver.com>
2022-11-09 21:18:40 +0900

    message

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,20 @@
 import Head from "next/head";
 import Styles from "../styles/Layout.module.css"
-import Slider from "react-slick";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { signOut, useSession } from 'next-auth/react';
+import { Menu } from '@headlessui/react';
+import Cookies from 'js-cookie';
 
 export default function Layout({ title, children }) {
 
-  const settings = {
-    arrows: true,
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  };
+  const { status, data: session } = useSession()
+
+  const logoutClickHandler = () => {
+    Cookies.remove('cart')
+    signOut({ callbackUrl: '/signin'})
+  }
+
 
   return (
     <>
@@ -24,71 +24,82 @@ export default function Layout({ title, children }) {
         <link rel="icon" href="/festival_decoration_party_celebration_holiday_flag_carnival_icon_150775.ico" />
       </Head>
 
+      <ToastContainer position="bottom-center" limit={1} />
+      
       <div className={Styles.bar}>
+      
         <header>
-          <nav className={Styles.links}>
-          
+          <nav className="flex flex-row h-12 items-center px-4 justify-between shadow-md">
+            
             <div className={Styles.gitimgbox}>
               <img className={Styles.gitimg} src="images/logo.png"></img>
             </div>
-
-              <a href="/" className="">
+              <a href="/" className="text-lg font-bold absolute pl-14">
               Let's Frestival</a>
-              <a href="/">공지사항</a>
-              <a href="/">축재소개</a>
-              <a href="/">커뮤니티</a>
-              <a href="/">로그인</a>
-              <a href="/">Contect</a>
-            </nav>
+              <div>
+                <a href="/" className="p-2">공지사항</a>
+                <a href="/" className="p-2">축재소개</a>
+                <a href="/" className="p-2">커뮤니티</a>
+                <a href="/" className="p-2">개발자 연결</a>
+
+                {status === 'loading' ? ('연결중..') : 
+                session?.user ? ( 
+                <Menu as="div" className="relative inline-block text-left">
+                  <Menu.Button className="inline-flex w-full justify-center rounded-md
+                  bg-black bg-opacity-30 px-4 py-2 text-sm font-medium
+                  text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2
+                  focus-visible:ring-white focus-visible:ring-opacity-75">
+                  {session.user.name}
+                  </Menu.Button>
+
+                  <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y
+                  divide-gray-100 rounded-md bg-white shadow-lg ring-1
+                  ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+
+                    <Menu.Item>
+                      {({ active }) => (
+                      <button
+                        className={`${
+                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        href="/">
+                          설정
+                      </button>
+                      )}
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      {({ active }) => (
+                      <button
+                        className={`${
+                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        href="logout"
+                        onClick={logoutClickHandler}>
+                          로그아웃
+                      </button>
+                      )}
+                    </Menu.Item>
+
+                    </div>
+                  </Menu.Items>
+                </Menu>
+                ) : (
+                <a href="/signin" className="p-2">로그인</a>
+                )}
+              </div>
+          </nav>
         </header>
       </div>
 
-    <div className={Styles.slidecenter}>
-      <div className={Styles.slidemain}>
-        <div className={Styles.slide}>
-          <Slider {...settings}>
-            <div className={Styles.slideimgbox}>
-              <h3>
-                <img className={Styles.slideimg} src="images/001.jpg"></img>
-              </h3>
-            </div>
-            <div className={Styles.slideimgbox}>
-              <h3>
-                <img className={Styles.slideimg} src="images/002.jpg"></img>
-              </h3>
-            </div>
-            <div className={Styles.slideimgbox}>
-              <h3>
-                <img className={Styles.slideimg} src="images/003.jpg"></img>
-              </h3>
-            </div>
-            <div className={Styles.slideimgbox}>
-              <h3>
-                <img className={Styles.slideimg} src="images/004.jpg"></img>
-              </h3>
-            </div>
-            <div className={Styles.slideimgbox}>
-              <h3>
-              <img className={Styles.slideimg} src="images/005.jpg"></img>
-              </h3>
-            </div>
-            <div className={Styles.slideimgbox}>
-              <h3>
-              <img className={Styles.slideimg} src="images/006.jpg"></img>
-              </h3>
-            </div>
-          </Slider>
-        </div>
-      </div>
-      </div>
-
         <main className="container m-auto mt-4">{children}</main>
-
-        <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
+        
+        <br />
+        <footer className="flex h-10 justify-center items-center shadow-inner bg-green-200">
           <p className={Styles.footer}>Copyright &copy; 2022 Powered by Team FallGuys</p>
         </footer>
 
-      
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import '../styles/globals.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
 import Script from "next/script"
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+
   return (
     <>
+    <SessionProvider session ={session}>
       <Script
-        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=08d2f8ea91351e2cdff88adc5567ac0f&libraries=services,clusterer&autoload=false"
+        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=66a5cde096ffa869666f3a8c941b7e5e&libraries=services,clusterer&autoload=false"
         strategy="beforeInteractive"
       />
       <Component {...pageProps} />
+    </SessionProvider>
     </>
   )
 }

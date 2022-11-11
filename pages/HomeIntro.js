3dc7f793e593b7f83352557163d5853455bf092f
JBUpark21 <bzpark@naver.com>
2022-11-12 02:09:20 +0900

    message

--- pages/HomeIntro.js
+++ pages/HomeIntro.js
@@ -6,8 +6,8 @@ export default function HomeIntro() {
   return (
     <Layout title="HomeIntro">
       <div>
-        <img className={styles.Image} src="images/HomeIntro.png"></img>
-        <img className={styles.Image} src="images/HomeIntro2.png"></img>
+        <img className={styles.Image} src="images/HomeIntro.jpg"></img>
+        <img className={styles.Image} src="images/HomeIntro2.jpg"></img>
       </div>
       <div className={styles.card_box}></div>
       <h1 className={styles.MainFont}>홈페이지 소개</h1>
@@ -19,14 +19,17 @@ export default function HomeIntro() {
         <br />
         <br />
         <br />
+        <br />
         다양한 정보들을 하나의 사이트에서 손쉽게 얻어 축제들에 대한 관심도를
         높일 수 있습니다.
         <br />
         <br />
         <br />
         <br />
+        <br />
         그리고 축제 주변의 숙박시설과 음식시설을 다른 링크와 연계하여 여행
         계획을 더 쉽게 짤 수 있습니다.
+        <br />
         <br />
         <br />
         <br />

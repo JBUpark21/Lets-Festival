b3d54f65720e6e939d9a8acb17b9c598194b8142
JBUpark21 <bzpark@naver.com>
2022-12-10 02:15:15 +0900

    message

--- pages/crypto/jwt-storage.js
+++ pages/crypto/jwt-storage.js
@@ -1,40 +1,58 @@
-import React, { useState } from "react";
-import Layout from "../../components/Layout";
-import jwt from "jsonwebtoken";
-import axios from "axios";
-import { useSession } from "next-auth/react";
+import React, { useState } from 'react';
+import Layout from '../../components/Layout';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import Cookies from 'js-cookie';
 
-export default function JwtScreen() {
+export default function JwtStorageScreen() {
   const { data: session } = useSession();
   const userEmail = session?.user.email;
 
   const [username, setUsername] = useState(userEmail);
-  const [token, setToken] = useState("");
-  const [decoded, setDecoded] = useState("");
-  const [result, setResult] = useState("");
+  const [jwtLocal, setJwtLocal] = useState('');
+  const [jwtSession, setJwtSession] = useState('');
+  const [jwtCookie, setJwtCookie] = useState('');
 
   const genJwt = async () => {
-    await axios.post("/api/crypto/jwt", { username }).then((res) => {
-      setToken(res.data.token);
-      localStorage.setItem("jwt", res.data.token);
-      let decoded = jwt.decode(res.data.token);
-      setDecoded(JSON.stringify(decoded));
-    });
-  };
+    await axios.post('/api/crypto/jwt-storage', { username }).then((res) => {
+      const token = res.data.token;
 
-  const verifyJwt = async () => {
-    await axios.post("/api/crypto/jwt-v", { token }).then((res) => {
-      setResult(res.data.result);
+      // 로컬스토리지에 저장
+      localStorage.setItem('jwt', token);
+      // 세션스토리지에 저장
+      sessionStorage.setItem('jwt', token);
+      // 쿠키에 저장
+      Cookies.set('jwt', token);
+
+      console.log('Local storage: ' + localStorage.getItem('jwt'));
+      console.log('Session storage: ' + sessionStorage.getItem('jwt'));
+      console.log('Cookie: ' + Cookies.get('jwt'));
+
+      // HttpOnly 쿠키는 클라이언트에서 읽을 수 없음
+      console.log('Secure Cookie: ' + Cookies.get('sToken'));
+
+      // 로컬스토리지에서 읽어옴
+      setJwtLocal(localStorage.getItem('jwt'));
+      // 세션스토리지에서 읽어옴
+      setJwtSession(sessionStorage.getItem('jwt'));
+      // 쿠키에서 읽어옴
+      setJwtCookie(Cookies.get('jwt'));
     });
   };
 
   return (
     <Layout title="JWT">
       <form className="mx-auto max-w-screen-lg">
-        <h1 className="text-3xl mb-4 font-bold">JSON Web Token (JWT)</h1>
-        <p>
-          JSON Web Tokens are an open, industry standard RFC 7519 method for
-          representing claims securely between two parties.
+        <h1 className="text-3xl mb-4 font-bold">JSON Web Token - Storage </h1>
+        <p className="mb-4">
+          <b>브라우저에서 사용할 수 있는 스토리지 종류</b> <br />
+          LocalStorage - 지우기 전까지는 계속 저장됨. 브라우저가 읽고 쓰기
+          가능함. <br />
+          SessionStorage - 브라우저가 읽고 쓰기 가능함. 세션이 끝나면 자동으로
+          지워짐. <br />
+          Cookie - 쿠키에 저장된 정보는 서버에게 전송시 항상 자동 첨부됨. <br />
+          HttpOnly Cookie - 서버가 저장하고 서버만 읽을 수 있는 쿠키.
+          클라이언트에서는 접근 불가.
         </p>
 
         <div className="mb-4">
@@ -64,7 +82,7 @@ export default function JwtScreen() {
 
         <div className="mb-4">
           <label htmlFor="jwt" className="mb-3 font-bold">
-            JWT - server issued
+            JWT - LocalStorage
           </label>
           <textarea
             name="jwt"
@@ -72,52 +90,40 @@ export default function JwtScreen() {
             cols="50"
             rows="3"
             className="w-full bg-gray-50"
-            value={token}
+            value={jwtLocal}
             readOnly
           ></textarea>
         </div>
 
         <div className="mb-4">
           <label htmlFor="jwt" className="mb-3 font-bold">
-            JWT - client decoded, no verification
+            JWT - SessionStorage
           </label>
           <textarea
             name="jwt"
             id="jwt"
             cols="50"
-            rows="2"
+            rows="3"
             className="w-full bg-gray-50"
-            value={decoded}
+            value={jwtSession}
             readOnly
           ></textarea>
         </div>
 
         <div className="mb-4">
-          <button
-            className="primary-button w-full"
-            type="button"
-            onClick={verifyJwt}
-          >
-            Verify JWT
-          </button>
-        </div>
-
-        <div className="mb-4">
           <label htmlFor="jwt" className="mb-3 font-bold">
-            Verified by server
+            JWT - Cookie
           </label>
-          <input
+          <textarea
             name="jwt"
             id="jwt"
+            cols="50"
+            rows="3"
             className="w-full bg-gray-50"
-            value={result ? "valid token" : "invalid"}
+            value={jwtCookie}
             readOnly
-          ></input>
+          ></textarea>
         </div>
-      </form>
-
-      <form>
-        
       </form>
     </Layout>
   );

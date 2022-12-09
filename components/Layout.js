e99a1bf2baf84d0f81980ba38450d9657a8e49b9
JBUpark21 <bzpark@naver.com>
2022-12-10 02:57:38 +0900

    message

--- components/Layout.js
+++ components/Layout.js
@@ -26,80 +26,80 @@ export default function Layout({ title, children }) {
 
       <header
         aria-label="Site Header"
-        class="border-b border-gray-100 z-20 fixed bg-white w-full shadow-lg"
+        className="border-b border-gray-100 z-20 fixed bg-white w-full shadow-lg"
       >
-        <div class="mx-auto flex h-16 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8">
-          <div class="flex items-center">
-            <button type="button" class="p-2 sm:mr-4 lg:hidden">
+        <div className="mx-auto flex h-16 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8">
+          <div className="flex items-center">
+            <button type="button" className="p-2 sm:mr-4 lg:hidden">
               <svg
-                class="h-6 w-6"
+                className="h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h16M4 18h16"
                 />
               </svg>
             </button>
 
-            <Link href="/" class="flex">
+            <Link href="/" className="flex">
               <div>
-                <img src="/logo.png" class="h-16"></img>
+                <img src="/logo.png" className="h-16"></img>
               </div>
             </Link>
           </div>
 
-          <div class="flex flex-1 items-center justify-end">
+          <div className="flex flex-1 items-center justify-end">
             <nav
               aria-label="Site Nav"
-              class="hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-wide lg:text-gray-500"
+              className="hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-wide lg:text-gray-500"
             >
               <Link
                 href="/Fmain"
-                class="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
               >
                 축제 소개
               </Link>
 
               <Link
                 href="/Boardindex"
-                class="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
               >
                 게시판
               </Link>
 
               <Link
                 href="/contact"
-                class="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
               >
                 문의하기
               </Link>
 
               <Link
                 href="/about"
-                class="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
               >
                 개발자
               </Link>
               <Link
                 href="/crypto/jwt"
-                class="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
+                className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-orange-400 mx-4"
               >
                 Jwt
               </Link>
             </nav>
 
-            <div class="ml-8 flex items-center">
-              <div class="flex items-center divide-x divide-gray-100 border-x border-gray-100">
+            <div className="ml-8 flex items-center">
+              <div className="flex items-center divide-x divide-gray-100 border-x border-gray-100">
                 <span>
                   {status === 'loading' ? (
                     <a
-                      className="p-2 inline-flex justify-center rounded-md
+                      classNameName="p-2 inline-flex justify-center rounded-md
             bg-orange-400 bg-opacity-80 px-4 py-2 text-sm font-medium
             text-white hover:bg-opacity-100 focus:outline-none focus-visible:ring-2
             focus-visible:ring-white focus-visible:ring-opacity-75"
@@ -107,9 +107,12 @@ export default function Layout({ title, children }) {
                       연결중..
                     </a>
                   ) : session?.user ? (
-                    <Menu as="div" className="relative inline-block text-left">
+                    <Menu
+                      as="div"
+                      classNameName="relative inline-block text-left"
+                    >
                       <Menu.Button
-                        className="inline-flex w-full justify-center rounded-md
+                        classNameName="inline-flex w-full justify-center rounded-md
             bg-orange-400 bg-opacity-80 px-4 py-2 text-sm font-medium
             text-white hover:bg-opacity-100 focus:outline-none focus-visible:ring-2
             focus-visible:ring-white focus-visible:ring-opacity-75
@@ -119,15 +122,15 @@ export default function Layout({ title, children }) {
                       </Menu.Button>
 
                       <Menu.Items
-                        className="absolute right-0 mt-2 w-56 origin-top-right divide-y
+                        classNameName="absolute right-0 mt-2 w-56 origin-top-right divide-y
             divide-gray-100 rounded-md bg-white shadow-lg ring-1
             ring-black ring-opacity-5 focus:outline-none"
                       >
-                        <div className="px-1 py-1 ">
+                        <div classNameName="px-1 py-1 ">
                           <Menu.Item>
                             {({ active }) => (
                               <Link
-                                className={`${
+                                classNameName={`${
                                   active
                                     ? 'bg-orange-400 text-white'
                                     : 'text-gray-900'
@@ -136,12 +139,12 @@ export default function Layout({ title, children }) {
                               >
                                 {active ? (
                                   <Cog6ToothIcon
-                                    className="mr-2 h-5 w-5"
+                                    classNameName="mr-2 h-5 w-5"
                                     aria-hidden="true"
                                   />
                                 ) : (
                                   <Cog6ToothIcon
-                                    className="mr-2 h-5 w-5"
+                                    classNameName="mr-2 h-5 w-5"
                                     aria-hidden="true"
                                   />
                                 )}
@@ -153,7 +156,7 @@ export default function Layout({ title, children }) {
                           <Menu.Item>
                             {({ active }) => (
                               <button
-                                className={`${
+                                classNameName={`${
                                   active
                                     ? 'bg-orange-400 text-white'
                                     : 'text-gray-900'
@@ -163,12 +166,12 @@ export default function Layout({ title, children }) {
                               >
                                 {active ? (
                                   <ArrowUturnRightIcon
-                                    className="mr-2 h-5 w-5"
+                                    classNameName="mr-2 h-5 w-5"
                                     aria-hidden="true"
                                   />
                                 ) : (
                                   <ArrowUturnRightIcon
-                                    className="mr-2 h-5 w-5"
+                                    classNameName="mr-2 h-5 w-5"
                                     aria-hidden="true"
                                   />
                                 )}
@@ -182,7 +185,7 @@ export default function Layout({ title, children }) {
                   ) : (
                     <Link
                       href="/signin"
-                      className="p-2 inline-flex justify-center rounded-md
+                      classNameName="p-2 inline-flex justify-center rounded-md
             bg-orange-400 bg-opacity-80 px-4 py-2 text-sm font-medium
             text-white hover:bg-opacity-100 focus:outline-none focus-visible:ring-2
             focus-visible:ring-white focus-visible:ring-opacity-75"
@@ -197,15 +200,15 @@ export default function Layout({ title, children }) {
         </div>
       </header>
 
-      <main className="">{children}</main>
+      <main classNameName="">{children}</main>
 
-      <footer className="bg-orange-100">
-        <div class="py-3 uk-link-reset">
-          <div class="flex flex-col items-center justify-between lg:flex-row max-w-6xl mx-auto lg:space-y-0 space-y-3">
-            <div class="flex space-x-2 text-gray-700 uppercase">
+      <footer classNameName="bg-orange-100">
+        <div className="py-3 uk-link-reset">
+          <div className="flex flex-col items-center justify-between lg:flex-row max-w-6xl mx-auto lg:space-y-0 space-y-3">
+            <div className="flex space-x-2 text-gray-700 uppercase">
               <Link href="/about">About</Link>
             </div>
-            <p class="capitalize">
+            <p className="capitalize">
               Copyright &copy; 2022 Powered by Team FallGuys
             </p>
           </div>

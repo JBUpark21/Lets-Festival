e0c2a8030d89a02b2b3c1e18da24229d8bb29dca
JBUpark21 <bzpark@naver.com>
2022-12-11 21:52:00 +0900

    message

--- pages/festival/[slug].js
+++ pages/festival/[slug].js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -163,22 +164,23 @@ export default function FestivalScreen() {
                 target="blank"
                 href={`https://map.kakao.com/link/map/${festival.location},${festival.lat},${festival.lng}`}
                 class="mt-8 inline-flex items-center rounded border border-indigo-600 bg-indigo-600 px-8 py-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-              ></Link>
-              <span class="text-sm font-medium">지도 크게보기</span>
-              <svg
-                class="ml-3 h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
+                <span class="text-sm font-medium">지도 크게보기</span>
+                <svg
+                  class="ml-3 h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+              </Link>
             </div>
           </div>
         </div>
